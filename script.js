e9ddcb33d111cae8eb53507566823c2013cef4f7
Lucas <lucasrian.dev@gmail.com>
2026-02-21 21:36:06 -0300

    refazendo logica

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-const input = document.querySelector("#input-busca");
+const listaDeMoedas = document.querySelector("#lista-moedas");
 
-async function Api() {
-  const moeda = input.value;
-  if (moeda === "") return;
-  const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${moeda}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+let TodasAsCriptos = [];
+let moedaAtual = null;
 
+async function api() {
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   try {
     const resposta = await fetch(url);
-
     if (!resposta.ok) {
-      throw new Error("ERROOOOO");
+      throw new Error("ERROOOO");
     }
 
     const dados = await resposta.json();
     console.log(dados);
+
+    moedaAtual = dados;
   } catch (error) {
     console.log(error);
-    alert("Api deu erro!");
   }
+}
+
+api();
+
+function renderizar() {
+  listaDeMoedas.innerHTML = "";
+
+  listaDeMoedas.forEach((moeda) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+            <td>${moeda.market_cap_rank}</td>
+            
+            <td>
+                <div class="moeda-info">
+                    <img src="${moeda.image}" alt="${moeda.name}">
+                    <span>${moeda.name} <small>${moeda.symbol}</small></span>
+                </div>
+            </td>
+            
+            <td>R$ ${moeda.current_price}</td>
+            
+            <td>${moeda.price_change_percentage_24h}%</td>
+            
+            <td>R$ ${moeda.market_cap}</td>
+        `;
+
+    listaDeMoedas.appendChild(tr);
+  });
 }
